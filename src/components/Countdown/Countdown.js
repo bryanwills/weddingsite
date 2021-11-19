d06de3fbc0cc0e9d0005cd7f8e38710d5baca568
Rob Little <rob@littlelogic.ca>
2021-11-19 15:16:58 -0800

    fixing bug in countdown clock

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -9,13 +9,10 @@ export class Countdown extends React.Component {
     };
   }
 
-  weddingTime = new Date(2022, 12, 28, 16, 0);
-
-  componentWillMount() {
-    this.getTimeUntil();
-  }
+  weddingTime = new Date(2022, 11, 28, 16, 0);
 
   componentDidMount() {
+    this.getTimeUntil();
     this.interval = setInterval(() => this.getTimeUntil(), 1000);
   }
 
@@ -25,7 +22,7 @@ export class Countdown extends React.Component {
 
   getTimeUntil = () => {
     const currentTime = new Date();
-    const weddingTime = new Date(2022, 12, 28, 16, 0);
+    const weddingTime = new Date(2022, 11, 28, 16, 0);
     const timeUntil = weddingTime - currentTime;
 
     // const seconds = Math.floor((timeUntil / 1000) % 60);
