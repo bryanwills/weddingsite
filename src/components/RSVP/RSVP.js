f6c03764c81e52c9f74eab6ab0911660aed1ab19
Rob Little <rob@littlelogic.ca>
2021-12-01 20:23:43 -0800

    trying to fix geo not talking, package.json old amplify-cli version update

--- src/components/RSVP/RSVP.js
+++ src/components/RSVP/RSVP.js
@@ -14,8 +14,8 @@ import { Card } from 'react-bootstrap';
                 <br/>
                 {t("rsvp.wedding_planner_info")}
                 </Card.Text>
-                <a href="mailto:karina@verdementa.com.co?subject=Maria Del Mar y Robert Edward Burton Boda"><img src="img/mail.png" height="30" alt="EMail"/>Karina Avendaño</a>.<br/>
-                <a href="https://api.whatsapp.com/send?phone=573152199077"><img src="img/WhatsApp-clear.png" height="50" alt="WhatsApp Logo"/>+57 315 219 9077</a>
+                <Card.Link><a href="mailto:karina@verdementa.com.co?subject=Maria Del Mar y Robert Edward Burton Boda"><img src="img/mail.png" height="30" alt="EMail"/>Karina Avendaño</a>.</Card.Link><br/>
+                <Card.Link><a href="https://api.whatsapp.com/send?phone=573152199077"><img src="img/WhatsApp-clear.png" height="50" alt="WhatsApp Logo"/>+57 315 219 9077</a></Card.Link>
             </div>
         </div>
       );
