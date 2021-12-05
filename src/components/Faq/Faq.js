479e497686aeca1ebfd4555850f2b90c8242376c
Rob Little <rob@littlelogic.ca>
2021-12-05 11:21:48 -0800

    hyperlinks in faq page

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import Image from 'react-bootstrap/Image'
 import { Card } from 'react-bootstrap';
 
@@ -16,16 +16,17 @@ import { Card } from 'react-bootstrap';
           </div>
           <div className="lefted">
             <br/>
+            
             <br/><h3>{t("faq.q1")}</h3>
             {t("faq.a1")}<br/>
-            <br/><h3>{t("faq.q2", {avianca:'<a href="www.avianca.com">avianca</a>'})}</h3>
-            {t("faq.a2")}<br/>
+            <br/><h3>{t("faq.q2")}</h3>
+            <Trans i18nKey="faq.a2" t={t} components={[<a href="https://www.avianca.com/" target="_blank"/>,<a href="https://www.despegar.com.co/" target="_blank"/>]} /><br/>
             <br/><h3>{t("faq.q3")}</h3>
             {t("faq.a3")}<br/>
             <br/><h3>{t("faq.q4")}</h3>
             {t("faq.a4")}<br/>
             <br/><h3>{t("faq.q5")}</h3>
-            {t("faq.a5")}<br/>
+            <Trans i18nKey="faq.a5" t={t} components={[<a href="https://www.oanda.com/currency-converter" target="_blank"/>]} /><br/>
             <br/><h3>{t("faq.q6")}</h3>
             {t("faq.a6")}<br/>
             <br/><h3>{t("faq.q7")}</h3>
