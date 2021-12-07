17d92fb36bec50532588e91c6cf46e0f55a4d7b8
Rob Little <rob@littlelogic.ca>
2021-12-06 18:23:36 -0800

    working map, adding locations

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -2,7 +2,6 @@ import React  from "react";
 import { Accordion } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Image } from 'react-bootstrap';
-import { Card } from 'react-bootstrap';
 import Map from '../../components/Map/Map';
 
 const Location = () => {
@@ -10,56 +9,40 @@ const Location = () => {
     return (
       <div className="" id="home">
           <div className="center">
-          <Card.Body>
-            <div className="header">
-              {t("location.main")}<br/>
-              {t("location.main2")}<br/>
-            </div>
-            <div className="header">
-              {/* <Image src="img/location.png" fluid/><br/> */}
-              {/* <Map/> */}
-              <Map id="map"/>
-            </div>
-              <Accordion>
-                <Accordion.Item eventKey="0">
-                  <Accordion.Header><b>Bello Horizonte</b></Accordion.Header>
-                  <Accordion.Body>
-                  <div className="header"><Image src="img/bellohorizante.png" fluid/><br/></div>
-                    {t("location.belo.main")}
-                    <br/>
-                    <br/>
-                    {t("location.belo.hotel_labels")}<br/>
-                    <br/>
-                    <a href="https://www.zuana.com/">{t("location.belo.zuana")}</a>,<br/>
-                    <a href="https://www.irotama.com/">{t("location.belo.irotama")}</a>,<br/>
-                    <a href="https://www.marriott.com/hotels/travel/smrmc-santa-marta-marriott-resort-playa-dormida/?gclid=Cj0KCQiAtJeNBhCVARIsANJUJ2EfybDkiPmehueu7lRkMyGxCcaZMXu43Qknnalc0aejwXxzTkuP2o4aAlN3EALw_wcB&gclsrc=aw.ds">{t("location.belo.marriott")}</a>,<br/>
-                    <a href="https://planetofhotels.com/en/colombia/santa-marta/apartamento-en-sintana-resort">{t("location.belo.sintana")}</a><br/>
-                    <br/>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                  <Accordion.Header><b>Pozos Colorados</b></Accordion.Header>
-                  <Accordion.Body>
-                  <div className="header"><Image src="img/pozos.png" fluid/><br/></div>
-                    {t("location.pozo.main")}
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                  <Accordion.Header><b>El Rodadero</b></Accordion.Header>
-                  <Accordion.Body>
-                  <div className="header"><Image src="img/El_Rodadero.jpg" fluid/><br/></div>
-                    {t("location.elro.main")}
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                  <Accordion.Header><b>Santa Marta {t("location.sant.santa")}</b></Accordion.Header>
-                  <Accordion.Body>
-                  <div className="header"><Image src="img/santamarta1.png" fluid/><br/></div>
-                    {t("location.sant.main")}
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </Card.Body>
+            <div class="container">
+              <div className="row">
+                <div className="col">
+                  <div className="padding">
+                    <div className="">
+                      {t("location.main")}<br/>
+                      {t("location.main2")}<br/>
+                    </div>
+                      <Map id="map"/>
+                      <br/>
+                  </div>
+                </div>
+                <div className="col-md-auto">
+                  locations panels coming soon
+                {/* <ul id="locations">
+                    <li><b>Bello Horizonte</b><br/><Image src="img/bellohorizante.png" fluid/><br/>    
+                      <div className="black">{t("location.belo.main")}</div>
+                      <br/>
+                      <br/>
+                      <div className="black">{t("location.belo.hotel_labels")}<br/></div>
+                      <br/>
+                      <a href="https://www.zuana.com/">{t("location.belo.zuana")}</a>,<br/>
+                      <a href="https://www.irotama.com/">{t("location.belo.irotama")}</a>,<br/>
+                      <a href="https://www.marriott.com/hotels/travel/smrmc-santa-marta-marriott-resort-playa-dormida/?gclid=Cj0KCQiAtJeNBhCVARIsANJUJ2EfybDkiPmehueu7lRkMyGxCcaZMXu43Qknnalc0aejwXxzTkuP2o4aAlN3EALw_wcB&gclsrc=aw.ds">{t("location.belo.marriott")}</a>,<br/>
+                      <a href="https://planetofhotels.com/en/colombia/santa-marta/apartamento-en-sintana-resort">{t("location.belo.sintana")}</a><br/>
+                      <br/>
+                    </li>
+                    <li><b>Pozos Colorados</b><br/><Image src="img/pozos.png" fluid/><br/><div className="black">{t("location.pozo.main")}</div></li>
+                    <li><b>El Rodadero</b><br/><Image src="img/El_Rodadero.jpg" fluid/> <br/> <div className="black">{t("location.elro.main")}</div></li>
+                    <li><b>Santa Marta {t("location.sant.santa")}</b><br/><Image src="img/santamarta1.png" fluid/><br/><div className="black">{t("location.sant.main")}</div></li>
+                  </ul> */}
+                </div>
+              </div>
+            </div>   
           </div>
     </div>
     );
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,36 +1,88 @@
 import { useEffect, useRef } from "react";
-import { createMap } from "maplibre-gl-js-amplify";
+import { createMap, drawPoints } from "maplibre-gl-js-amplify";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 // Santa Marta, Colombia
 const INITIAL_VIEWPORT = {
-  longitude: 11.24079,
-  latitude: -74.19904
+  latitude: 11.24079,
+  longitude: -74.19904
 }
 
 function Map() {
   const mapRef = useRef(null); // Reference to the map DOM element
 
   // Wrapping our code in a useEffect allows us to run initializeMap after the div has been rendered into the DOM
-  useEffect(() => {
-    let map;
-    async function initializeMap() {
-      // We only want to initialize the underlying maplibre map after the div has been rendered
-      if (mapRef.current != null) {
-        map = await createMap({
-          container: mapRef.current,
-          center: [INITIAL_VIEWPORT.latitude, INITIAL_VIEWPORT.longitude],
-          zoom: 11,
-        });
-      }
-    }
-    initializeMap();
+  useEffect( async() => {
+    
+    const map = await initializeMap();
+    addWeddingLocations(map);
 
     // Cleans up and maplibre DOM elements and other resources - https://maplibre.org/maplibre-gl-js-docs/api/map/#map#remove
     return function cleanup() {
       if (map != null) map.remove();
     };
   }, []);
+
+  async function initializeMap() {
+    // We only want to initialize the underlying maplibre map after the div has been rendered
+    if (mapRef.current != null) {
+      const map = await createMap({
+        container: mapRef.current,
+        center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
+        zoom: 10,
+      });
+      return map;
+    }
+  }
+
+  function addWeddingLocations(map) {
+    map.on("load", function () {
+      drawPoints("mySourceName", // Arbitrary source name
+          [
+              {
+                coordinates: [-74.23259, 11.117275],
+                title: "Airport",
+                address: "Avenida Aeropuerto Km. 80, Local LO-11-012A, Santa Marta, Bolivar, Colombia",
+              },
+              {
+                coordinates: [-74.2108436, 11.1391886],
+                title: "Mamancana",
+                address: "Km 5 Vía al Puerto, Santa Marta, Magdalena, Colombia",
+              },
+              {
+                coordinates: [-74.224777, 11.145841],
+                title: "Bello Horizonte",
+                address: "Santorini Villas, Calle 121A, La Paz, 470006, MAG, Colombia",
+              },
+              {
+                coordinates: [-74.209572, 11.170727],
+                title: "Pozos Colorados",
+                address: "Pozos Colorados, 005075 Santa Marta, MAG, Colombia",
+              },
+              {
+                coordinates: [-74.227908, 11.202656],
+                title: "El Rodadero",
+                address: "El Rodadero, Calle 12, Peñon del Rodadero, 470005 Santa Marta, MAG, Colombia",
+              },
+              {
+                coordinates: [-74.212807, 11.245051],
+                title: "Santa Marta (dt)",
+                address: "Juan Valdez, Calle 14, Comuna 2, Santa Marta, MAG, Colombia",
+              },
+          ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
+          map,
+          {
+              showCluster: true,
+              unclusteredOptions: {
+                  showMarkerPopup: true,
+              },
+              clusterOptions: {
+                  showCount: true,
+              },
+          }
+      );
+    });
+  }
 
   return <div ref={mapRef} className="map" id="map" />;
 }
